07100c1f38602e0be3f24c135add2a3dbe8254cb
David Fernando Santa <santadavid88@hotmail.com>
2021-02-13 21:21:48 -0500

    Delete Task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { isEmpty } from 'lodash'
 import React, {useState} from 'react'
+import {size} from 'lodash'
 import shortid from 'shortid'
+
 
 
 function App() {
@@ -25,6 +27,13 @@ function App() {
      setTask("")
    }
 
+   const deleteTask=(id)=>{
+
+    const filteredTasks=tasks.filter(t=>t.id!==id)
+    console.log(filteredTasks)
+    setTasks(filteredTasks)
+   }
+
 
   return ( 
     <div className="container mt-5">
@@ -33,17 +42,32 @@ function App() {
       <div className="row">
         <div className="col-8">
         <h4 className="text-center">Lista Tareas</h4>
-         <ul className="list-group">
-           {
-             tasks.map((t)=>(
-               <li className="list-group-item" key={t.id}>
-                 <span className="lead">{t.name}</span>
-                 <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
-                 <button className="btn btn-info btn-sm float-right">Editar</button>
-               </li>
-             ))
-           }
-         </ul>
+{
+     size(tasks)==0 ?
+     (
+   <h5>No hay tareas</h5>
+     ):
+     (
+<ul className="list-group">
+  {
+    tasks.map((item)=>(
+      <li className="list-group-item" key={item.id}>
+        <span className="lead">{item.name}</span>
+        <button className="btn btn-danger btn-sm float-right mx-2" onClick={()=>deleteTask(item.id)}>
+           Eliminar
+         </button>
+        <button className="btn btn-info btn-sm float-right">
+          Editar
+        </button>
+      </li>
+    ))
+  }
+</ul>
+     )
+  
+}
+       
+
         </div>
         <div className="col-4">
            <h4 className="text-center">Formulario</h4>
