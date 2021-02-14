4a9a0e00fbfd7cf1c8810efafb1fb9e778605451
David Fernando Santa <santadavid88@hotmail.com>
2021-02-13 22:05:15 -0500

    Finis Crud Memory

--- src/App.js
+++ src/App.js
@@ -1,90 +1,131 @@
-import { isEmpty } from 'lodash'
-import React, {useState} from 'react'
-import {size} from 'lodash'
-import shortid from 'shortid'
-
-
+import { isEmpty } from "lodash";
+import React, { useState } from "react";
+import { size } from "lodash";
+import shortid from "shortid";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [id, setId] = useState("");
+  const [error, setError] = useState(null);
 
-  const [task, setTask]=useState("")
-  const [tasks, setTasks]=useState([])
+  const validForm = () => {
+    let isValid = true;
+    setError(null);
+    if (isEmpty(task)) {
+      setError("Debes ingresar una taréa");
+      isValid = false;
+    }
 
-   const addTask=(e)=>{
-    e.preventDefault()
-     if(isEmpty(task)){
-       console.log("Task Vacio")
-       return
-     }
+    return isValid;
+  };
 
-     const newTaksk ={
-       id:shortid.generate(),
-       name:task
-     }
-    
-     setTasks([ ...tasks, newTaksk])
-     console.log(tasks)
-     setTask("")
-   }
+  const addTask = (e) => {
+    e.preventDefault();
 
-   const deleteTask=(id)=>{
+    if (!validForm()) {
+      return;
+    }
 
-    const filteredTasks=tasks.filter(t=>t.id!==id)
-    console.log(filteredTasks)
-    setTasks(filteredTasks)
-   }
+    const newTaksk = {
+      id: shortid.generate(),
+      name: task,
+    };
 
+    setTasks([...tasks, newTaksk]);
+    console.log(tasks);
+    setTask("");
+  };
 
-  return ( 
+  const deleteTask = (id) => {
+    const filteredTasks = tasks.filter((t) => t.id !== id);
+    console.log(filteredTasks);
+    setTasks(filteredTasks);
+  };
+
+  const editTask = (item) => {
+    setTask(item.name);
+    setEditMode(true);
+    setId(item.id);
+  };
+
+  const saveTask = (e) => {
+    e.preventDefault();
+    if (!validForm()) {
+      return;
+    }
+
+    const editedTaks = tasks.map((item) =>
+      item.id === id ? { id, name: task } : item
+    );
+    setTasks(editedTaks);
+    setEditMode(false);
+    setTask("");
+    setId("");
+  };
+
+  return (
     <div className="container mt-5">
-  
-      <hr/>
+      <hr />
       <div className="row">
         <div className="col-8">
-        <h4 className="text-center">Lista Tareas</h4>
-{
-     size(tasks)==0 ?
-     (
-   <h5>No hay tareas</h5>
-     ):
-     (
-<ul className="list-group">
-  {
-    tasks.map((item)=>(
-      <li className="list-group-item" key={item.id}>
-        <span className="lead">{item.name}</span>
-        <button className="btn btn-danger btn-sm float-right mx-2" onClick={()=>deleteTask(item.id)}>
-           Eliminar
-         </button>
-        <button className="btn btn-info btn-sm float-right">
-          Editar
-        </button>
-      </li>
-    ))
-  }
-</ul>
-     )
-  
-}
-       
-
+          <h4 className="text-center">Lista Tareas</h4>
+          {size(tasks) == 0 ? (
+           <li className="list-group-item"> No hay tareas programadas</li>
+          ) : (
+            <ul className="list-group">
+              {tasks.map((item) => (
+                <li className="list-group-item" key={item.id}>
+                  <span className="lead">{item.name}</span>
+                  <button
+                    className="btn btn-danger btn-sm float-right mx-2"
+                    onClick={() => deleteTask(item.id)}
+                  >
+                    Eliminar
+                  </button>
+                  <button
+                    className="btn btn-info btn-sm float-right"
+                    onClick={() => editTask(item)}
+                  >
+                    Editar
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
         <div className="col-4">
-           <h4 className="text-center">Formulario</h4>
-           <form onSubmit={addTask}>
-             <input
+          <h4 className="text-center">
+            {editMode ? "Modificar Taréa" : "Agragar Tarea"}
+          </h4>
+          <form onSubmit={editMode ? saveTask : addTask}>
+           {
+             error && <span className="text-danger">{error}</span>
+           }
+            <input
               type="text"
               className="form-control mb-2"
               placeholder="Ingrese la taréa.."
-              onChange={(text)=>setTask(text.target.value)}
+              onChange={(text) => setTask(text.target.value)}
               value={task}
-             />
-                <button className="btn btn-dark btn-block" type="submit">Agregar</button>
-           </form>
+            />
+         
+            <button
+              className={
+                editMode
+                  ? "btn btn-warning btn-block"
+                  : "btn btn-dark btn-block"
+              }
+              type="submit"
+            >
+              {editMode ? "Guardar" : "Agregar"}
+            </button>
+          </form>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
